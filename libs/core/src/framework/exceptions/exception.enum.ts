0750df213706306d4257c7922d971ad7448f2355
manhcao <manhcaoduy1912@gmail.com>
2022-04-17 23:53:25 +0700

    fix error_codes name

--- libs/core/src/framework/exceptions/exception.enum.ts
+++ libs/core/src/framework/exceptions/exception.enum.ts
@@ -4,6 +4,6 @@ export enum Statuses {
 }
 
 export const ERROR_CODES = {
-  PROBLEM_WITH_REQUEST: 'mosh_bad_request',
-  SERVER_ERROR: 'mosh_server_error',
+  PROBLEM_WITH_REQUEST: 'backend_bad_request',
+  SERVER_ERROR: 'backend_server_error',
 };
